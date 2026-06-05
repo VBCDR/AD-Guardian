5601108fc06d751077b953999cdae9c53fe8df1f
Cian Rogers <cian.rogers@funasset.com>
2026-06-05 12:38:30 +0100

    Bump version to 2.0.3

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,9 +1,9 @@
 using System.Reflection;
 using System.Windows;
 
-[assembly: AssemblyVersion("2.0.2.0")]
-[assembly: AssemblyFileVersion("2.0.2.0")]
-[assembly: AssemblyInformationalVersion("2.0.2")]
+[assembly: AssemblyVersion("2.0.3.0")]
+[assembly: AssemblyFileVersion("2.0.3.0")]
+[assembly: AssemblyInformationalVersion("2.0.3")]
 
 [assembly:ThemeInfo(
     ResourceDictionaryLocation.None,            //where theme specific resource dictionaries are located
